e9b4e85e622d24f987793c03985f6b96391ca638
Andriana Skaperdas <askap@bu.edu>
2020-04-02 12:02:57 -0400

    added urls update

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -7,5 +7,5 @@ from .views import ShowAllProfilesView, ShowProfilePageView
 
 urlpatterns = [
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
-    path('profile/<int:pk>', ShowProfilePageView.as_view(), name='profile'),
+    path('profile/<int:pk>', ShowProfilePageView.as_view(), name='show_profile_page'),
 ]
